1d615da199cb5115075cccbbf8302f680ab6a1d1
deijaparker <deijaparker1@outlook.com>
2024-08-30 18:11:35 -0700

    Add development notes outlining steps for setting up Express backend

---
+++ appNotes.js
@@ -0,0 +1,25 @@
+// ---[Steps for Backend-Express]
+// 1. npm init -y
+// 2. npm install express , nodemon
+// 3. Create server.js [Main]
+// 4. Create "start": script in package.json
+// ------------------------[Establish Server (5)]
+// --> [express()-- app()-- listen()]
+// ------------------------[Setup_MVC structure]
+// --> 6. Make models ,views, controllers, routes
+// {models}
+// ----------> Data
+// {views}
+// -----------> Pages/Components
+// npm install express ejs --save
+// {controllers*}
+
+// ------------------------[Control and Manage Data -(models)]
+// 7. imports/exports
+// ------------------------[Establish Routes]
+// ---> using app.[method*] create route PER MODEL
+
+// ------------------------[Create REST/CRUD]
+// * -> [GET,POST,PUT(PATCH),DELETE] --- RESTful Routes
+// * -> Create, Read, Update, Delete -- CRUD
+// ---> Repeat this structure for all new models.
